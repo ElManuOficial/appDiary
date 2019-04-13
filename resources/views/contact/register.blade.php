96fd20c8f525fe7797e7a3ba7340cfc2319dfd7f
joe <joedarwin1998@gmail.com>
2019-04-13 17:05:38 -0400

    final segunda clase views

---
+++ resources/views/contact/register.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Formulario de Creación de Usuarios</div>
+
+                {{-- Tarea crear formulario y mascara co jquey mask --}}
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
